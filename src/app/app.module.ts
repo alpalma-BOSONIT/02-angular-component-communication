d7ec193643e78483dcfce1ce79d7c581996e08be
Álvaro Palma Mancheño <alvaro.palma@bosonit.com>
2022-11-18 11:13:47 +0100

    cleanup imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './components/parent/parent.component';
 import { ChildComponent } from './components/child/child.component';
-import { MyServiceService } from './services/my-service.service';
 
 @NgModule({
   declarations: [AppComponent, ParentComponent, ChildComponent],
